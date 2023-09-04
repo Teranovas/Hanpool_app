c3f05a11175756de624c3ef0366e6900b9385892
Teranovas <goteb123@hs.ac.kr>
2023-09-05 02:03:07 +0900

    채팅방 진짜완료

--- app/src/main/java/com/example/joinn/chatfragment/ChatRoomFragment.java
+++ app/src/main/java/com/example/joinn/chatfragment/ChatRoomFragment.java
@@ -1,5 +1,7 @@
 package com.example.joinn.chatfragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -47,6 +50,8 @@ public class ChatRoomFragment extends Fragment {
     private FirebaseAuth mAuth;
     private DatabaseReference mDbRef;
 
+    private DatabaseReference usersRef;
+
     private String receiverRoom;
     private String senderRoom;
 
@@ -54,6 +59,15 @@ public class ChatRoomFragment extends Fragment {
 
     private ImageView ProfileImageView;
     private TextView NicknameTextView;
+
+    private Button inviteBtn;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        usersRef = FirebaseDatabase.getInstance().getReference().child("users");
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -69,6 +83,8 @@ public class ChatRoomFragment extends Fragment {
 
         ProfileImageView = view.findViewById(R.id.profileImageView);
         NicknameTextView = view.findViewById(R.id.nicknameTextView);
+
+        inviteBtn = view.findViewById(R.id.inviteBtn);
 
         Bundle arguments = getArguments();
         if (arguments != null) {
@@ -105,22 +121,50 @@ public class ChatRoomFragment extends Fragment {
                 String getTime = dateFormat.format(date);
 
                 String message = binding.messageEdit.getText().toString();
-                Message messageObject = new Message(message, senderUid, getTime, me);
-                messageList.add(messageObject);
-                messageAdapter.notifyDataSetChanged();
+                usersRef.child(senderUid).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        String Nickname = snapshot.child("닉네임").getValue(String.class);
+                        Message messageObject = new Message(message, senderUid, getTime, Nickname);
+                        messageList.add(messageObject);
+                        messageAdapter.notifyDataSetChanged();
 
-                // 데이터 저장
-                // 데이터 저장
-                mDbRef.child("chats").child(senderRoom).child("messages").push()
-                        .setValue(messageObject)
-                        .addOnSuccessListener(aVoid -> {
-                            // 저장 성공하면
-                            mDbRef.child("chats").child(receiverRoom).child("messages").push()
-                                    .setValue(messageObject)
-                                    .addOnSuccessListener(aVoid2 -> {
-                                        // 받는 쪽에도 저장 성공하면 어댑터 갱신
-                                    });
-                        });
+                        // 데이터 저장
+                        // 데이터 저장
+                        mDbRef.child("chats").child(senderRoom).child("messages").push()
+                                .setValue(messageObject)
+                                .addOnSuccessListener(aVoid -> {
+                                    // 저장 성공하면
+                                    mDbRef.child("chats").child(receiverRoom).child("messages").push()
+                                            .setValue(messageObject)
+                                            .addOnSuccessListener(aVoid2 -> {
+                                                // 받는 쪽에도 저장 성공하면 어댑터 갱신
+                                            });
+                                });
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+//                Message messageObject = new Message(message, senderUid, getTime, me);
+//                messageList.add(messageObject);
+//                messageAdapter.notifyDataSetChanged();
+//
+//                // 데이터 저장
+//                // 데이터 저장
+//                mDbRef.child("chats").child(senderRoom).child("messages").push()
+//                        .setValue(messageObject)
+//                        .addOnSuccessListener(aVoid -> {
+//                            // 저장 성공하면
+//                            mDbRef.child("chats").child(receiverRoom).child("messages").push()
+//                                    .setValue(messageObject)
+//                                    .addOnSuccessListener(aVoid2 -> {
+//                                        // 받는 쪽에도 저장 성공하면 어댑터 갱신
+//                                    });
+//                        });
 
 
                 // 입력값 초기화
@@ -147,6 +191,41 @@ public class ChatRoomFragment extends Fragment {
                         System.out.println("Error: " + error.getMessage());
                     }
                 });
+        inviteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // AlertDialog를 생성합니다.
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle("날짜 선택");
+
+                // CalendarView를 생성하고 대화 상자에 추가합니다.
+                CalendarView calendarView = new CalendarView(getContext());
+                builder.setView(calendarView);
+
+                // OK 버튼을 추가하고 클릭 리스너를 설정합니다.
+                builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // 여기서 선택한 날짜를 처리하거나 저장할 수 있습니다.
+                        long selectedDateMillis = calendarView.getDate();
+                        // 선택한 날짜에 대한 작업을 수행합니다.
+                    }
+                });
+
+                // Cancel 버튼을 추가하고 클릭 리스너를 설정합니다.
+                builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // 취소 버튼을 눌렀을 때 수행할 작업을 추가할 수 있습니다.
+                        dialogInterface.dismiss();
+                    }
+                });
+
+                // AlertDialog를 표시합니다.
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
 
 
         return view;
