eda6ea488f74d207166080d14678ee80ef6c1e48
Teranovas <125176569+Teranovas@users.noreply.github.com>
2023-09-06 16:48:38 +0900

    꾸미기1

--- app/src/main/java/com/example/joinn/splash/introSplashActivity.java
+++ app/src/main/java/com/example/joinn/splash/introSplashActivity.java
@@ -1,10 +1,16 @@
 package com.example.joinn.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.joinn.R;
 import com.example.joinn.matchingActivity;
@@ -12,7 +18,16 @@ import com.example.joinn.matchingActivity;
 
 public class introSplashActivity extends AppCompatActivity {
 
+    private ConstraintLayout container;
+
     private static final String TAG = "introSplashActivity";
+
+    private TextView text1;
+    private TextView text2;
+    private TextView text3;
+
+    private int currentTextView = 1;
+
 
 
     @Override
@@ -20,15 +35,39 @@ public class introSplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro_splash);
 
-        new Handler().postDelayed(new Runnable() {
+        text1 = findViewById(R.id.Text1);
+        text2 = findViewById(R.id.Text2);
+        text3 = findViewById(R.id.Text3);
+
+        View.OnClickListener clickListener = new View.OnClickListener() {
             @Override
-            public void run() {
-                Intent intent = new Intent(introSplashActivity.this, matchingActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                startActivity(intent);
-                finish();
-            }}, 3000);
+            public void onClick(View view) {
+                // 현재 보이는 텍스트뷰를 다음 텍스트뷰로 전환하면서 애니메이션 적용
+                switch (currentTextView) {
+                    case 1:
+                        showWithAnimation(text2);
+                        currentTextView = 2;
+                        break;
+                    case 2:
+                        showWithAnimation(text3);
+                        currentTextView = 3;
+                        break;
+                    case 3:
+                        currentTextView = 1;
+                        break;
+                }
+            }
+        };
 
+        // 아무 화면이나 클릭할 때마다 클릭 이벤트 처리
+        findViewById(android.R.id.content).setOnClickListener(clickListener);
+    }
 
+    // 텍스트뷰를 페이드 인 애니메이션과 함께 나타나게 함
+    private void showWithAnimation(final TextView textView) {
+        AlphaAnimation alphaAnimation = new AlphaAnimation(0.0f, 1.0f);
+        alphaAnimation.setDuration(1000);
+        alphaAnimation.setFillAfter(true);
+        textView.startAnimation(alphaAnimation);
     }
 }
